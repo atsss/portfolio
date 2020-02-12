3eebb95a8c03331fe9a9bcf9e79e1fe9f4e22575
atsss <atsushi0623itoh@gmail.com>
2020-02-12 14:46:01 -0600

    create header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Layout } from 'antd';
+import { Typography } from 'antd';
 import './App.sass';
 
 const { Header, Content, Footer } = Layout;
+const { Title } = Typography;
 
 const App = () => (
   <Layout className="layout">
     <Header>
-      <div className="logo" />
+      <Title style={{ color: '#fff', lineHeight: '64px' }}>Atsushi's Portfolio</Title>
     </Header>
     <Content className="content">
       <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
     </Content>
-    <Footer className="footer">Atsushi's portfolio ©2020 Created by Atsushi</Footer>
+    <Footer className="footer">Atsushi's Portfolio ©2020 Created by Atsushi</Footer>
   </Layout>
 );
 
