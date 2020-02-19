d9e307dbf23b103cc5c01518373545c2683a6b41
atsss <atsushi0623itoh@gmail.com>
2020-02-19 11:26:52 -0600

    add qiita airticles to works

--- src/data/works.js
+++ src/data/works.js
@@ -1,5 +1,6 @@
 import payment from '../assets/images/payment.jpg';
 import search from '../assets/images/search.jpg';
+import qiita from '../assets/images/qiita.png';
 
 const works = [
   {
@@ -21,6 +22,26 @@ const works = [
       "I researched UX. Then I did coding web app and UI design."
     ],
     link: "https://docs.google.com/document/d/1sj17XyrbsAV-m15hzrt3lX7wtsEazl7t4VbVQTbIyQw/edit?usp=sharing"
+  },
+  {
+    title: "How to do UX design for engineers",
+    image: { alt: "qiita", src: qiita },
+    paragraphs: [
+      "I wrote the ariticle about UX design for software engineers",
+      "The ariticle is a summary of the Human Centered Design class I took in AIIT",
+      "and focuses on how engineers do it with developing.(only Japanese)"
+    ],
+    link: "https://qiita.com/Atsss/items/3b610e20fa8d5f830b40"
+  },
+  {
+    title: "How to reduce CSS files with FLOCSS",
+    image: { alt: "qiita", src: qiita },
+    paragraphs: [
+      "I wrote the ariticle about designing CSS with FLOCSS",
+      "The ariticle tells you difficulties to design CSS",
+      "and tips to reduce CSS files and manage the project.(only Japanese)"
+    ],
+    link: "https://qiita.com/Atsss/items/4f9d98fb1d0546539c09"
   }
 ];
 
