bae631bf0a2e23a62b1459b5504e34b04260937c
atsss <atsushi0623itoh@gmail.com>
2020-02-19 11:47:40 -0600

    add logme airticles to works

--- src/data/works.js
+++ src/data/works.js
@@ -1,24 +1,45 @@
 import payment from '../assets/images/payment.jpg';
 import search from '../assets/images/search.jpg';
+import presentation from '../assets/images/presentation.jpg';
 import qiita from '../assets/images/qiita.png';
 
 const works = [
   {
+    title: "Tips to do both of UI design and development",
+    image: { alt: "presentation", src: presentation },
+    paragraphs: [
+      "This is the airticle about the presentation I did.",
+      "After I released Local Works Payment, I decided to share the tips to design UI with atomic design",
+      "and how to code it efficiently with React.(only Japanese)"
+    ],
+    link: "https://logmi.jp/tech/articles/320774"
+  },
+  {
     title: "Local Works Payment",
     image: { alt: "payment", src: payment },
     paragraphs: [
-      "Local Works Payment is a QR payment service for small and medium-sized enterprises in the Japanese construction industry.",
-      "I was in charge of development and UI/UX design.",
-      "I researched UX. Then I did coding web/native app and UI design."
+      "Local Works Payment is a QR payment service for small and medium-sized enterprises in the Japanese",
+      "construction industry. I was in charge of development and ui/ux design.",
+      "I researched UX. then I did coding web/native app and UI design."
     ],
-    link: "https://docs.google.com/document/d/1a9eFUsWiF2P6MPvbS-NG2DVKteF5CIzDLYXUAG-1nzA/edit?usp=sharing"
+    link: "https://docs.google.com/document/d/1a9efuswif2p6mpvbs-ng2dvktef5cizdlyxuag-1nza/edit?usp=sharing"
+  },
+  {
+    title: "Tips to research UX from nothing",
+    image: { alt: "presentation", src: presentation },
+    paragraphs: [
+      "This is the airticle about the presentation I did.",
+      "After I released Local Works Search, I decided to share the tips to research UI and interview users.",
+      "I shared common failures when making something new as well.(only Japanese)"
+    ],
+    link: "https://logmi.jp/tech/articles/320290"
   },
   {
     title: "Local Works Search",
     image: { alt: "search", src: search },
     paragraphs: [
-      "Local Works Search is a job matching service for small and medium-sized enterprises in the Japanese construction industry.",
-      "I was in charge of development and UI/UX design.",
+      "Local Works Search is a job matching service for small and medium-sized enterprises in the Japanese",
+      "construction industry. I was in charge of development and UI/UX design.",
       "I researched UX. Then I did coding web app and UI design."
     ],
     link: "https://docs.google.com/document/d/1sj17XyrbsAV-m15hzrt3lX7wtsEazl7t4VbVQTbIyQw/edit?usp=sharing"
