dd5309f75ca535aa8f749098472c8a090a10a9ea
atsss <atsushi0623itoh@gmail.com>
2020-02-19 11:04:06 -0600

    create works data

--- src/components/organisms/Works/index.js
+++ src/components/organisms/Works/index.js
@@ -2,33 +2,9 @@ import React from 'react';
 import Section from '../../atoms/Section';
 import Work from "../../molecules/Work";
 import { Typography } from 'antd';
-import payment from '../../../assets/images/payment.jpg';
-import search from '../../../assets/images/search.jpg';
+import works from '../../../data/works';
 
 const { Title } = Typography;
-
-const works = [
-  {
-    title: "Local Works Payment",
-    image: { alt: "payment", src: payment },
-    paragraphs: [
-      "Local Works Payment is a QR payment service for small and medium-sized enterprises in the Japanese construction industry.",
-      "I was in charge of development and UI/UX design.",
-      "I researched UX. Then I did coding web/native app and UI design."
-    ],
-    link: "https://docs.google.com/document/d/1a9eFUsWiF2P6MPvbS-NG2DVKteF5CIzDLYXUAG-1nzA/edit?usp=sharing"
-  },
-  {
-    title: "Local Works Search",
-    image: { alt: "search", src: search },
-    paragraphs: [
-      "Local Works Search is a job matching service for small and medium-sized enterprises in the Japanese construction industry.",
-      "I was in charge of development and UI/UX design.",
-      "I researched UX. Then I did coding web app and UI design."
-    ],
-    link: "https://docs.google.com/document/d/1sj17XyrbsAV-m15hzrt3lX7wtsEazl7t4VbVQTbIyQw/edit?usp=sharing"
-  }
-]
 
 const Works = () => (
   <Section>
---
+++ src/data/works.js
@@ -0,0 +1,27 @@
+import payment from '../assets/images/payment.jpg';
+import search from '../assets/images/search.jpg';
+
+const works = [
+  {
+    title: "Local Works Payment",
+    image: { alt: "payment", src: payment },
+    paragraphs: [
+      "Local Works Payment is a QR payment service for small and medium-sized enterprises in the Japanese construction industry.",
+      "I was in charge of development and UI/UX design.",
+      "I researched UX. Then I did coding web/native app and UI design."
+    ],
+    link: "https://docs.google.com/document/d/1a9eFUsWiF2P6MPvbS-NG2DVKteF5CIzDLYXUAG-1nzA/edit?usp=sharing"
+  },
+  {
+    title: "Local Works Search",
+    image: { alt: "search", src: search },
+    paragraphs: [
+      "Local Works Search is a job matching service for small and medium-sized enterprises in the Japanese construction industry.",
+      "I was in charge of development and UI/UX design.",
+      "I researched UX. Then I did coding web app and UI design."
+    ],
+    link: "https://docs.google.com/document/d/1sj17XyrbsAV-m15hzrt3lX7wtsEazl7t4VbVQTbIyQw/edit?usp=sharing"
+  }
+];
+
+export default works;
